e982e028cb5cd0b7286bbe8fc1a392d132d3f844
pgrandieres <54800217+pgrandieres@users.noreply.github.com>
2024-01-03 17:59:18 +0100

    Create DumDate.ts

---
+++ DumDate.ts
@@ -0,0 +1,34 @@
+function isObjectNotDate(value: any): value is Record<string, unknown> {
+  return (
+    typeof value === 'object' && value !== null && !(value instanceof Date)
+  );
+}
+export function convertIsoDateStrings<T>(data: T): T {
+  // Handle the case where data is a string and might be an ISO date
+  if (typeof data === 'string') {
+    if (/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d+)?Z$/.test(data)) {
+      return DateTime.fromISO(data) as T;
+    }
+
+    return data;
+  }
+
+  // Handle the case where data is an array
+  if (Array.isArray(data)) {
+    return data.map((item) => convertIsoDateStrings(item)) as T;
+  }
+
+  // Handle the case where data is an object but not null or a Date instance
+  if (isObjectNotDate(data)) {
+    const newData = {} as any;
+    Object.keys(data).forEach((key) => {
+      const value = data[key];
+      newData[key] = convertIsoDateStrings(value);
+    });
+
+    return newData as T;
+  }
+
+  // Return data as is for other types like number, null, etc.
+  return data;
+}
